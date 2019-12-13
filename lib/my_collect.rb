37df99006005fe7014673e4b2d62b8d8742ce5c3
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-13 18:02:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect (array)
+  index = 0 
+  while index < array.length 
+  yield(array[index])
+  index += 1
+end
+return
 
+end
+languages  = ["ruby", "javascript", "python", "objective-c"]
 
