4c30cb6133391cedae496c39189cdfc1da473566
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-13 18:30:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect (array)
+  index = 0 
+  modified_array = []
+  while index < array.length 
+ modified_array << yield(array[index])
+  index += 1
+end
+modified_array
+end
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+  language.upcase
+end
 
-
+students = ["Tim Jones", "Tom holloway", "Sophie Black", "Antoin White"]
+my_collect(students) do |student|
+  student.split(" ").first
+end
